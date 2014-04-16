896a9fe9e4ff796593e0b41d01f14f5001eb2320
Jan Vlnas <git@jan.vlnas.cz>
2014-04-16 15:28:19 +0200

    Migrate from codepen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,50 @@
-console.log('\'Allo \'Allo!');
+(function(document){
+  var WRAPPER_ID = 'fitbar';
+  var LINKS = [
+    {
+      title: 'FIT',
+      url: 'https://fit.cvut.cz/'
+    },
+    {
+      title: 'KOS',
+      url: 'https://kos.is.cvut.cz/'
+    },
+    {
+      title: 'Edux',
+      url: 'https://edux.fit.cvut.cz/'
+    },
+    {
+      title: 'Timetable',
+      url: 'https://timetable.fit.cvut.cz/'
+    },
+    {
+      title: 'GitLab',
+      url: 'https://gitlab.fit.cvut.cz/'
+    }
+  ];
+
+
+  var generateNav = function(linksList) {
+    var anchors = linksList.map(function(link){
+      return '<a href="'+link.url+'">'+link.title+'</a>';
+    })
+
+    var nav = document.createElement('nav');
+    nav.innerHTML = anchors.join('');
+    nav.className = 'links';
+    return nav;
+  }
+
+  var generateBar = function() {
+    var bar = document.createElement('div');
+
+    bar.id = WRAPPER_ID;
+    bar.appendChild(generateNav(LINKS));
+
+    return bar;
+  }
+
+  document.body.appendChild(generateBar());
+
+
+})(document);
