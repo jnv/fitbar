0e0b9ad53ca98eb2c3b4e2abc05544259ce68359
Jan Vlnas <git@jan.vlnas.cz>
2014-11-03 17:15:00 +0100

    Setup initial bar injection

--- src/bar/index.js
+++ src/bar/index.js
@@ -4,13 +4,13 @@ var links = require('./links');
 
 //controller class
 function controller() {
-  controller.links = links;
+  this.links = links;
 }
 
 //view class
 function view(ctrl) {
   function link(data) {
-    return m('a.fitbar-Link', {href: data.url});
+    return m('a.fitbar-Link', {href: data.url, title: data.desc}, data.title);
   }
 
   return m('.fitbar-Bar', [
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 var m = require('mithril');
 var bar = require('./bar');
 
-m.module(document.body, bar);
+var root = document.createElement('div');
+m.module(root, bar);
+
+document.addEventListener('DOMContentLoaded', function inject() {
+  document.body.insertBefore(root, document.body.firstElementChild);
+});
