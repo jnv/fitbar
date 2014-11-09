c3dfefffc2cbc195333036433d998037d8cef65d
Jan Vlnas <git@jan.vlnas.cz>
2014-11-09 19:46:32 +0100

    Check for current url

--- src/bar/index.js
+++ src/bar/index.js
@@ -1,16 +1,34 @@
 'use strict';
 var m = require('mithril');
-var links = require('./links');
+
+function linksModel() {
+  var links = require('./links');
+
+  var location = window.location.hostname;
+
+  for (var i = links.length - 1; i >= 0; i--) {
+    var l = links[i];
+    if(~l.url.indexOf(location)) {
+      l.current = true;
+      break;
+    }
+  }
+
+  return links;
+}
+
+
 
 //controller class
 function controller() {
-  this.links = links;
+  this.links = linksModel();
 }
 
 //view class
 function view(ctrl) {
   function link(data) {
-    return m('a.fitbar-Link', {href: data.url, title: data.desc}, data.title);
+    var current = data.current ? '.is-current' : '';
+    return m('a.fitbar-Link' + current, {href: data.url, title: data.desc}, data.title);
   }
 
   return m('.fitbar-Bar', [
