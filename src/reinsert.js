5df2e0c7974fa0c62ac203b9be364067538e48d2
Jan Vlnas <git@jan.vlnas.cz>
2014-11-20 18:00:15 +0100

    Prepare reinsert

---
+++ src/reinsert.js
@@ -0,0 +1,22 @@
+'use strict';
+var pipe = require('functional-pipeline');
+var u = require('./util');
+
+var filterBody = function(el) {
+  return el && el.nodeName === 'BODY';
+};
+
+var addedBody = pipe(u.extractValue('addedNodes'), u.filter(filterBody));
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+  var lastAdded = u.last(mutations.map(addedBody));
+});
+
+
+// configuration of the observer:
+var mutationOptions = {childList: true, attributes: true};
+
+
+// pass in the target node, as well as the observer options
+observer.observe(document.documentElement, mutationOptions);
